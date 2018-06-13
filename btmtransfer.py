92c4a589278e24d209a5cb9214dafbc7c42b76c0
John Chi <john@8btc-1s-MBP.localdomain>
2018-06-13 10:59:23 +0800

    feat(input option): add account input option for get user wallet accountId

--- btmtransfer.py
+++ btmtransfer.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import getopt
 import httprequest
 
 
@@ -22,20 +23,31 @@ def validate_amount(line, amount):
 
 
 def validate_input(argv):
-    if len(argv) < 2 or len(argv) >= 2 and not str(argv[1]).endswith('.txt'):
-        print('Input error:Use this script with input txt file.'
-              + '\n' + 'Example usage:' + '\n' + '\t./btmtransfer.py input.txt')
+    global input_path
+    global account_id
+    try:
+        opts, args = getopt.getopt(argv[1:], 'a:i:', ['account=', 'input='])
+        if len(opts) < 2:
+            raise getopt.GetoptError('lose command option parameter')
+    except getopt.GetoptError as err:
+        print('Input error:' + str(err) +
+              '\n' + 'Example usage:' +
+              '\n' + '\t./btmtransfer.py -i input.txt -a 0BF63M2U00A04')
         sys.exit(1)
-
+    for opt, arg in opts:
+        if opt in ('-a', '--account'):
+            account_id = arg
+        if opt in ('-i', '--input'):
+            input_path = arg
     # relative path
-    path = os.path.abspath('.') + '/' + argv[1]
+    path = os.path.abspath('.') + '/' + input_path
     if not os.path.exists(path):
         # absolute path
-        path = argv[1]
+        path = input_path
     # read file
     with open(path, 'r', encoding='utf-8') as file:
         for line in file:
-            splits = line.split(',')
+            splits = line.strip().split(',')
             validate_address(line, splits[0])
             validate_amount(line, splits[1])
 
@@ -43,7 +55,7 @@ def validate_input(argv):
 def main(argv=None):
     if argv is None:
         argv = sys.argv
-        validate_input(argv)
+    validate_input(argv)
 
 
 if __name__ == "__main__":
